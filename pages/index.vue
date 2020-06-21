81f7cb17fa156fed0c08d97f1ce01012311b4c5f
Oliver Libutzki <oliver@libutzki.de>
2020-06-21 10:15:10 +0200

    Table added

--- pages/index.vue
+++ pages/index.vue
@@ -17,9 +17,13 @@
           Documentation
         </a>
       </div>
+      <div>
+        <b-table striped hover :items="items" />
+      </div>
     </div>
   </div>
 </template>
+
 
 <script lang="ts">
 import Vue from 'vue'
@@ -28,6 +32,16 @@ import Logo from '~/components/Logo.vue'
 export default Vue.extend({
   components: {
     Logo
+  },
+  data () {
+    return {
+      items: [
+        { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+        { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+        { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+        { age: 38, first_name: 'Jami', last_name: 'Carney' }
+      ]
+    }
   }
 })
 </script>
